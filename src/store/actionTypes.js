acf4e2fc5c464a70d85d2de1f72596c15bde89de
jinhuaYu <kjyujinhua@163.com>
2018-08-16 16:33:26 +0800

    deleteItem功能 actionType拆分

---
+++ src/store/actionTypes.js
@@ -0,0 +1,4 @@
+// actionType 拆分的好处， 写错action 会提示
+export const CHANGE_INPUT_VALUE = 'change_input_value'
+export const ADD_TODO_ITEM = 'add_todo_item'
+export const DELETE_TODO_ITEM = 'delete_todo_item'
